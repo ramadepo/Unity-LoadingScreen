384455c1845f612bf3a8812f10337c6bf9d1cad4
ramadepo <iammastercloud@gmail.com>
2018-06-26 16:08:22 +0800

    init the loading bar test repo

---
+++ Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelLoad : MonoBehaviour {
+
+	public GameObject loadingScreen;
+	public Slider progressSlider;
+	public TextMeshProUGUI progressText;
+
+	public void LoadLevel(int sceneIndex){
+		StartCoroutine (LoadAsynchronously (sceneIndex));
+	}
+
+	IEnumerator LoadAsynchronously(int sceneIndex){
+		loadingScreen.SetActive (true);
+		AsyncOperation operation = SceneManager.LoadSceneAsync (sceneIndex);
+
+		while (!operation.isDone) {
+			float progress = Mathf.Clamp01 (operation.progress / 0.9f);
+			Debug.Log (progress);
+
+			progressSlider.value = progress;
+			progressText.text = (progress * 100).ToString ("###") + " %";
+
+			yield return null;
+		}
+	}
+
+}
